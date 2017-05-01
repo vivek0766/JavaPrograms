9bdad4501bbb8b31fbcd6a8c1d29815d2ee63d2e
Vivek Reddy Komatireddy <k.vivekreddy0766@gmail.com>
2017-05-01 16:06:32 -0400

    Added Prime Number Program q

---
+++ JavaPrograms/src/main/java/com/csrk/primenumber/PrimeNumber.java
@@ -0,0 +1,24 @@
+package com.csrk.primenumber;
+
+import com.csrk.exceptions.NotAValidInputException;
+
+public class PrimeNumber {
+
+	public void prime(String value) throws NotAValidInputException{
+		int number = Integer.parseInt(value);
+
+		boolean isPrime = true;
+		//int temp;
+		for (int i = 2; i < number / 2; i++) {
+			//temp = ;
+			if (number % i == 0) {
+				isPrime = false;
+				break;
+			}
+		} if(isPrime)
+			System.out.println(number+" is Prime Number");
+		else
+		System.out.println(number+" is Not a prime Number");
+
+	}
+}
---
+++ JavaPrograms/src/main/java/com/csrk/primenumber/PrimeNumberApp.java
@@ -0,0 +1,19 @@
+package com.csrk.primenumber;
+
+import com.csrk.exceptions.NotAValidInputException;
+
+public class PrimeNumberApp {
+
+	public static void main(String[] args) {
+		PrimeNumber primeNum = new PrimeNumber();
+		try {
+			primeNum.prime(args[0]);
+		} catch (NotAValidInputException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			System.out.println("Not a valid Input..!");
+		}
+
+	}
+
+}
