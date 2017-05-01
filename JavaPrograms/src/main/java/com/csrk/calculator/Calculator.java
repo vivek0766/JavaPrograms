3e8c46fdb07f24f47a54d54d3150e9fdf605eba0
Vivek Reddy Komatireddy <k.vivekreddy0766@gmail.com>
2017-05-01 16:00:51 -0400

    Changes made in Calculator Program

--- JavaPrograms/src/main/java/com/csrk/calculator/Calculator.java
+++ JavaPrograms/src/main/java/com/csrk/calculator/Calculator.java
@@ -1,34 +1,37 @@
 package com.csrk.calculator;
 
+import com.csrk.exceptions.NotAValidInputException;
+
 //import java.util.Scanner;
 
 public class Calculator {
 
-	/*public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-         int a ;         int b  ;
-         int Addition, Substraction, Multiplication, Division;
-         
-         System.out.println("Enter value of A ->");
-         a = sc.nextInt();
-         System.out.println("Enter value of B ->");
-         b = sc.nextInt();
-          Addition = a+b;
-          Substraction = b-a;
-          Multiplication= a*b;
-          Division= a/b;
-          System.out.println("Addition of a and b is ->"+Addition);
-          System.out.println("Substraction of a and b-> "+Substraction);
-          System.out.println(" Multiplication of a and b->"+Multiplication);
-          System.out.println("Division of a and b->"+Division);
-          sc.close();
-	}*/
-	
-	int a, b=0;
-	
-	public void division(String value) throws ArithmeticException, NumberFormatException{
-		a = Integer.parseInt(value);
-		int c = a/b;
-		System.out.println("The value of C-->"+c);
+	/*
+	 * public static void main(String[] args) { Scanner sc = new
+	 * Scanner(System.in); int a ; int b ; int Addition, Substraction,
+	 * Multiplication, Division;
+	 * 
+	 * System.out.println("Enter value of A ->"); a = sc.nextInt();
+	 * System.out.println("Enter value of B ->"); b = sc.nextInt(); Addition =
+	 * a+b; Substraction = b-a; Multiplication= a*b; Division= a/b;
+	 * System.out.println("Addition of a and b is ->"+Addition);
+	 * System.out.println("Substraction of a and b-> "+Substraction);
+	 * System.out.println(" Multiplication of a and b->"+Multiplication);
+	 * System.out.println("Division of a and b->"+Division); sc.close(); }
+	 */
+
+	public void subtraction(String firstValue, String secondValue) throws NotAValidInputException {
+		int num1 = Integer.parseInt(firstValue);
+		int num2 = Integer.parseInt(secondValue);
+		int sub = num1 - num2;
+
+		System.out.println("The Subtaction of two Numbers -->" + sub);
+	} 
+
+	public void division(String number1, String number2) throws NotAValidInputException{
+		int first = Integer.parseInt(number1);
+		int second = Integer.parseInt(number2);
+		System.out.println("The result of division Numbers-->" + first / second);
+
 	}
 }
--- JavaPrograms/src/main/java/com/csrk/calculator/CalculatorApp.java
+++ JavaPrograms/src/main/java/com/csrk/calculator/CalculatorApp.java
@@ -1,15 +1,30 @@
 package com.csrk.calculator;
 
+import com.csrk.exceptions.NotAValidInputException;
+
 public class CalculatorApp {
 
-	public static void main(String[] args) {
-		Calculator calculator = new Calculator();
+	public static void main(String[] args) throws NotAValidInputException{
+		Calculator subtract = new Calculator();
 		try {
-			calculator.division(args[0]);
-		} catch (NumberFormatException exception) {
-			System.out.println("please give only numbers");
-		} catch (ArithmeticException exp) {
-			System.out.println("Any Number can not divisible by Zero");
+			subtract.subtraction(args[0], args[1]);
+		} catch (NumberFormatException exception) { 
+			System.out.println("please give only Integers");
+		}catch (ArrayIndexOutOfBoundsException exe) {
+			System.out.println("please give valid inputs");
+		} /*catch (NotAValidInputException e) {
+			//e.printStackTrace();
+		}*/
+		
+		Calculator divide = new Calculator();
+		try{
+		divide.division(args[2], args[3]);
+		}catch(ArrayIndexOutOfBoundsException exception){
+			System.out.println("Please give Correct input");
+		}catch(ArithmeticException ex){
+			System.out.println("The Numbers Can not be divisible by Zero");
+		}catch(NumberFormatException e){
+			System.out.println("Please give only Numbers");
 		}
 	}
 
