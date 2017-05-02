9970803f7a2352b73b5b29af309a094fed2426a3
Vivek Reddy Komatireddy <k.vivekreddy0766@gmail.com>
2017-05-02 15:52:46 -0400

    Small changes made in ArrayIndex Programs

--- JavaPrograms/src/main/java/com/csrk/stringargs/IntegerArray.java
+++ JavaPrograms/src/main/java/com/csrk/stringargs/IntegerArray.java
@@ -1,23 +1,21 @@
 package com.csrk.stringargs;
 
-
+import com.csrk.exceptions.NotAValidInputException;
 
 public class IntegerArray {
 	int[] array = { 2, 5, 4, 1, 5, 16, 7, 10, 9, 3 };
-   
-	public int findIndex(String value) throws ArrayIndexOutOfBoundsException{
+
+	public int findIndex(String value) throws NotAValidInputException, ArrayIndexOutOfBoundsException {
 		int number = Integer.parseInt(value);
-	   int indexofnum = -1;
-		for(int i =0; i<array.length;i++){
-			if(array[i]==number){
-				indexofnum=i;
-				System.out.println("Num Found");
-				break;
+		int indexofnum = -1;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == number) {
+				indexofnum = i;
+                break;
 			}
-				System.out.println("Num Not Found");
-			
+
 		}
-		
-     return array[indexofnum];
+
+		return indexofnum;
 	}
 }
--- JavaPrograms/src/main/java/com/csrk/stringargs/IntegerArrayIndex.java
+++ JavaPrograms/src/main/java/com/csrk/stringargs/IntegerArrayIndex.java
@@ -1,16 +1,24 @@
 package com.csrk.stringargs;
 
-
+import com.csrk.exceptions.NotAValidInputException;
 
 public class IntegerArrayIndex {
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 		IntegerArray indexofArray = new IntegerArray();
 		try {
-			indexofArray.findIndex(args[0]);
+			int indexofnum = indexofArray.findIndex(args[0]);
+			if (indexofnum == -1) {
+				System.out.println("The number is not present in array");
+			} else
+				System.out.println("The index of that number-->" + indexofnum);
 		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Please give correct input value ");
-			
+			System.out.println("Please give correct input value which is present in given Array");
+
+		} catch (NotAValidInputException exception) {
+			System.out.println("Enter Valid Input Array");
+		} catch (NumberFormatException exe) {
+			System.out.println("Please give correct format..! Only Integers");
 		}
 
 	}
