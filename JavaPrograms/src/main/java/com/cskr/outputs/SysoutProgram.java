a9f7c5d842d1d7ac2ee83804cb0e3649e784fd0c
VivekReddy <k.vivekreddy0766@gmail.com>
2019-05-07 15:26:04 -0400

    New commit on factiorial and Sysouts

---
+++ JavaPrograms/src/main/java/com/cskr/outputs/SysoutProgram.java
@@ -0,0 +1,27 @@
+package com.cskr.outputs;
+
+public class SysoutProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		//to print Star Pattern
+		System.out.println("    *    ");
+		System.out.println("* *   * *");
+		System.out.println("   * *   ");
+		System.out.println("  *   *  ");
+		System.out.println(" *     * ");
+
+		
+		System.out.println(" To Test Print and Println ");
+		
+		System.out.print(" First Name: ");
+		System.out.print("Vivek ");
+		System.out.print(" Reddy");
+		
+		System.out.print(" Last Name: ");
+		System.out.println("Komatireddy");
+	}
+
+}
